497530e66eafedc9227659281089ff6bb5eb78ea
Fr3_nn <162039270+Fr3-nn@users.noreply.github.com>
2024-06-10 13:04:22 +0400

    Add files via upload

---
+++ Handlers/SlashCommand.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const ascii = require('ascii-table');
+const table = new ascii('SlashCommands');
+table.setHeading('SlashCommand', 'Load Status');
+const client = require("./../Events/messageCreate")
+
+const SlashCommands = [];
+module.exports = client => {
+fs.readdirSync('./SlashCommands').forEach((folder) => {
+    const commandFiles = fs.readdirSync(`./SlashCommands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`../SlashCommands/${folder}/${file}`);
+        SlashCommands.push(command.data.toJSON())
+        if (command.data.name) {
+            client.SlashCommands.set(command.data.name, command);
+            table.addRow(file, '✅ ' + 'Success');
+        } else {
+            table.addRow(file, '❌ ' + 'Filed');
+            continue;
+        }
+    }
+});
+console.log(table.toString());
+    const rest = new REST({ version: '9' }).setToken(process.env.token);
+    (async () => {
+        try {
+            await rest.put(
+                Routes.applicationCommands(process.env.botID),
+                { body: SlashCommands },
+            );
+            console.log(`Succesfully registered ${SlashCommands.length} commands.`);
+        } catch (error) {
+            if (error) console.error(error);
+        }
+})();
+
+} 
